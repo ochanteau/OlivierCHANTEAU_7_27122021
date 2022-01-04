f454f0dfcfa5d1d18fd0bbe222c34132f5d17658
Olivier <chanteau.o@gmail.com>
2022-01-04 07:17:55 +0100

    modif de la persistance de la session avec le token dans le Lstorage

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,45 +2,50 @@ import { createStore } from 'vuex'
 
 const axios = require('axios');
 const instance = axios.create({baseURL: 'http://localhost:3000/api/auth'});
+instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`
+// let token = localStorage.getItem('token');
+// console.log("///////token");
+// console.log(token);
 
-let user = localStorage.getItem('user');
-if (!user) {
- user = {
-    userId: -1,
-    token: '',
-  }; 
-} else {
-  try {
-    user = JSON.parse(user);
-    instance.defaults.headers.common['Authorization'] = user.token;
-  } catch (err) {
-    user = {
-      userId: -1,
-      token: '',
-    };
-  }
-}
+// if (!token) {token = ""}
+// else {
+//   try {
+//     token = JSON.parse(token);
+//     instance.defaults.headers.common['Authorization'] =`Bearer ${token}`;
+//   } catch (err) {
+//     token = ''
+//     }
+// }
+
 
 console.log("////instance begin");
 console.log(instance.defaults.headers.common);
-console.log("////user");
-console.log(user);
+// console.log("////token");
+// console.log(token);
 
 export default createStore({
   state: {
-    user
+    user_id:"",
+    token :localStorage.getItem('token'),
+    isLoggedIn: localStorage.getItem('token')? true:false
   },
+  getters:{
+    isLoggedIn: state =>  state.isLoggedIn
+  },
+    
   mutations: {
     logUser : function(state,user){
-      console.log("user/////")
-      console.log(user)
-      instance.defaults.headers.common['Authorization'] = user.token;
-      console.log("////instance after");
-      console.log(instance.defaults.headers.common);
-      localStorage.setItem('user', JSON.stringify(user));
-      state.user =user;
-      console.log("state.user//////////")
-      console.log(state.user)
+        console.log("user/////")
+        console.log(user)
+      instance.defaults.headers.common['Authorization'] = `Bearer ${user.token}`;
+        console.log("////instance after");
+        console.log(instance.defaults.headers.common);
+      // localStorage.setItem('token', JSON.stringify(user.token));
+      localStorage.setItem('token', user.token);
+      state.token = user.token;
+      state.user_id =user.user_id;
+      state.isLoggedIn = true;
+        
     }
   },
   actions: {
