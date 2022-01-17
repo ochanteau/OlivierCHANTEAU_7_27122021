d8132f5fedff7a8616117ef003b1e2e021c999b6
Olivier <chanteau.o@gmail.com>
2022-01-17 10:27:34 +0100

    modification middleware verif owner pour integrer le moderateur

--- backend/middleware/OwnerValidation.js
+++ backend/middleware/OwnerValidation.js
@@ -10,53 +10,138 @@ exports.postVerification = (req, res, next) => {
   const {user_id} = req.token ;
   const post_id = req.params.id ;
   console.log(post_id)
-  // requete BDD sur la table post pour recuperer user_id du propriétaire
-  const sql = `SELECT user_id 
-               FROM post
-               WHERE post_id = ? `
-  db.query(sql, post_id, function(err, results) {
-      if (err){res.status(500).json({ err })}
+  // requete BDD sur la table post pour recupererles droits de l utilisateur
+  const sql = `SELECT droits_id
+               FROM user
+               WHERE user_id = ? `
+  db.query(sql, user_id, function(err, results) {
+    if (err){res.status(500).json({ err })}
+    else {
+      console.log(results[0].droits_id)
+      if (results && results[0].droits_id ==2)
+         { next();}
       else {
-        console.log(results[0].user_id)
-        if (!results || results[0].user_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
-        else {
-           // next ()
-        return res.status(200).json("ok");
-        }
-        
+      
+          // requete BDD sur la table post pour recuperer user_id du propriétaire
+        const sql2 = `SELECT user_id 
+        FROM post
+        WHERE post_id = ? `
+        db.query(sql2, post_id, function(err, results) {
+            if (err){res.status(500).json({ err })}
+            else {
+                  console.log(results[0].user_id)
+                if (!results || results[0].user_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
+              else {next ();}
+            }
+        }) 
       }
     }
-  )
+  })
+
+
+
+  // requete BDD sur la table post pour recuperer user_id du propriétaire
+  // const sql2 = `SELECT user_id 
+  //              FROM post
+  //              WHERE post_id = ? `
+  // db.query(sql2, post_id, function(err, results) {
+  //     if (err){res.status(500).json({ err })}
+  //     else {
+  //       console.log(results[0].user_id)
+  //       if (!results || results[0].user_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
+  //       else {
+  //          // next ()
+  //       return res.status(200).json("ok");
+  //       }
+        
+  //     }
+  //   }
+  // )
     
 };
 
 
 /*
 * middleware de comparaison de l'userId du token et 
-* de l'userId du propriétaire du post 
+* de l'userId du propriétaire du commentaire
 */
+
 exports.commentVerification = (req, res, next) => {
   
   const {user_id} = req.token ;
   const comment_id = req.params.id ;
   console.log(comment_id)
-  // requete BDD sur la table post pour recuperer user_id du propriétaire
-  const sql = `SELECT user_id 
-               FROM comments
-               WHERE comment_id = ? `
-  db.query(sql, comment_id, function(err, results) {
-      if (err){res.status(500).json({ err })}
+  // requete BDD sur la table post pour recupererles droits de l utilisateur
+  const sql = `SELECT droits_id
+               FROM user
+               WHERE user_id = ? `
+  db.query(sql, user_id, function(err, results) {
+    if (err){res.status(500).json({ err })}
+    else {
+      console.log(results[0].droits_id)
+      if (results && results[0].droits_id ==2)
+         {  // next ()
+          return res.status(200).json("ok");}
       else {
-        console.log(results[0].comment_id)
-        if (!results || results[0].comment_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
-        else {
-           // next ()
-        return res.status(200).json("ok");
+      
+          // requete BDD sur la table post pour recuperer user_id du propriétaire
+        const sql2 = `SELECT user_id 
+        FROM comments
+        WHERE comment_id = ? `
+        db.query(sql2, comment_id, function(err, results) {
+            if (err){res.status(500).json({ err })}
+            else {
+                  console.log(results[0].user_id)
+                if (!results || results[0].user_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
+              else {
+                    // next ()
+                    return res.status(200).json("ok");
+                  }
+
+            }
         }
-        
+        ) 
       }
     }
-  )
+  })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.commentVerification = (req, res, next) => {
+  
+//   const {user_id} = req.token ;
+//   const comment_id = req.params.id ;
+//   console.log(comment_id)
+//   // requete BDD sur la table post pour recuperer user_id du propriétaire
+//   const sql = `SELECT user_id 
+//                FROM comments
+//                WHERE comment_id = ? `
+//   db.query(sql, comment_id, function(err, results) {
+//       if (err){res.status(500).json({ err })}
+//       else {
+//         console.log(results[0].comment_id)
+//         if (!results || results[0].comment_id !=user_id) {res.status(403).json({ message:"Vous n'avez pas les droits necessaires"})}
+//         else {
+//            // next ()
+//         return res.status(200).json("ok");
+//         }
+        
+//       }
+//     }
+//   )
 
     
-};
+// }
