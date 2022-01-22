fe3c765b5d2a9a77c43006f3202899ce2ca06a5e
Olivier <chanteau.o@gmail.com>
2022-01-22 16:36:37 +0100

    clean controllers comment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -5,8 +5,7 @@ const db = require('../modele/database');
 
 // fonction qui renvoie la totalité des commentaires pour un post
 exports.getAllComments= (req, res, next)=> {
-
-  // requete BDD sur la table user join post et  envoie au client
+  // requete BDD sur la table user join comment et  envoie au client
   const post_id = req.params.id;
   const sql = `SELECT user.user_id,user_nom,user_prenom,user_picture,comment_id,comment_text,comment_date,post_id
                 FROM user
@@ -16,7 +15,6 @@ exports.getAllComments= (req, res, next)=> {
   db.query(sql,post_id, function(err, results) {
       if (err){res.status(500).json({ err })}
       else {
-        // console.log(results);
         return res.status(200).json(results);
       }
     }
@@ -29,21 +27,16 @@ exports.getAllComments= (req, res, next)=> {
 exports.createComment = (req, res, next) => {
   const {user_id} = req.token ;
   const post_id = req.params.id;
-  console.log(req.body.text);
   const comment_date = new Date();
-  console.log(comment_date)  
   const comment_text = req.body.text
-  console.log(comment_text)
   const comment = {comment_text,user_id,comment_date,post_id};
-  console.log(comment);
   // requete BDD sur la table user join post et  envoie au client
   const sql = `INSERT INTO comment SET  ? `
   db.query(sql,comment, function(err, results) {
     if (err){res.status(500).json({ err })}
     else {
-        console.log(results);
+        // recuperation du nouvelle id et envoie de l'objet comment au client
         comment.comment_id=results.insertId;
-        console.log(comment);
         return res.status(200).json(comment)
     }
   })
@@ -57,13 +50,12 @@ exports.createComment = (req, res, next) => {
   exports.deleteComment = (req, res, next) => {
     const {user_id} = req.token ;
     const comment_id = req.params.id ;
-    console.log(user_id, comment_id)
+    // requete de suppression en BDD
     const sql = `DELETE FROM comment
                     WHERE comment_id=? `
     db.query(sql,comment_id, function(err, results) {
         if (err){res.status(500).json({ err })}
         else {
-              console.log(results);
               return res.status(200).json( {message: "commentaire supprimé"})
         }
     })
@@ -78,20 +70,16 @@ exports.createComment = (req, res, next) => {
     exports.updateComment = (req, res, next) => {
       const comment_id = req.params.id ;
       comment_text = req.body.text  
-      console.log(comment_text)
+      // requete de mise à jour du commentaire en BDD
       const sql =`UPDATE comment
                    SET comment_text = ?
                    WHERE comment_id=? `  
       db.query(sql,[comment_text,comment_id], function(err, results) {
         if (err){res.status(500).json({ err })}
         else {
-            console.log(results);
             return res.status(200).json({comment_text})
         }
-      })     
-
-        
-              
+      })               
      
 };
 
