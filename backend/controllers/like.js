cfc91ada475c31ced3fd77ea4ee6eddb61ef5557
Olivier <chanteau.o@gmail.com>
2022-01-22 16:32:42 +0100

    clean ok controllers like

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -6,7 +6,6 @@ const db = require('../modele/database');
 // fonction qui renvoie la totalité des likes presents en BDD pour un post
 exports.getAllLike = (req, res, next)=> {
     const post_id = req.params.id;
-    console.log(post_id)
     // requete BDD sur la table like avec le post_id et  envoie au client
     const sql = `SELECT *
                 FROM groupomania.like
@@ -33,18 +32,17 @@ exports.createLike = (req, res, next)=> {
     db.query(sql,post_id,function(err, results) {
         if (err){res.status(500).json({ err })}
         else {
-            console.log(results)
+            // verification si l'utilisateur n' a pas déja liké le post
             const index = results.findIndex(el => el.user_id == user_id);
-            console.log(index)
             if (index==-1) {
                 const like = {post_id,user_id}
-                // requete BDD sur la table like avec le post_id et  envoie au client
+                // requete BDD creation du like
                 const sql = `INSERT INTO groupomania.like SET  ? `
                 db.query(sql,like,function(err, results) {
                     if (err){res.status(500).json({ err })}
-                    else {  console.log(results)
+                    else {  
+                            // insertion du nouvelle Id et envoie au client
                             like.like_id = results.insertId;
-                            console.log(like)
                             return res.status(200).json( {like} );
                     }
                 })
@@ -62,18 +60,13 @@ exports.createLike = (req, res, next)=> {
   exports.deleteLike = (req, res, next) => {
     const {user_id} = req.token ;
     const post_id = req.params.id ;
-    console.log(user_id, post_id)
-    // requete BDD sur la table post pour recupererles droits de l utilisateur
+    // requete BDD pour supprimer le like
     const sql = `DELETE FROM groupomania.like
                 WHERE post_id=? and user_id = ? `
     db.query(sql,[post_id,user_id], function(err, results) {
       if (err){res.status(500).json({ err })}
       else {
-        console.log(results);
-        return res.status(200).json( {message: "Post supprimé"})
-        
-            
-
+        return res.status(200).json( {message: "like supprimé"})
       }
     })
 
