9e333cd20781fad72021e19bf5fcf0ea5708c0b8
Olivier <chanteau.o@gmail.com>
2022-01-22 19:33:20 +0100

    clean et commentaires du composant Post

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,15 +1,17 @@
 <template>
  
 <div>
+      <!-- component de mise à jour du post -->
       <update-post v-if="this.update"
         :post="post"
         :index="index" 
         :updatePost="updatePost" 
         :openUpdatePost="openUpdatePost">
       </update-post>
-      <!-- affichage du corps du post -->
+      <!-- affichage du post -->
       <div  class="post" >
         <div class=" post__container">
+          <!-- partie avec les données du createur du post -->
           <div class="user">
               <img  class="user__profilPicture" height="50" width="50" :src="post.user_picture" alt="Image de profil ">
               <div  class="user__infos">
@@ -17,6 +19,7 @@
                 <p class="user__date">Le {{date(post.post_date)}} </p>
               </div>
           </div>
+          <!-- partie pour initier la MAJ ou la suppression du post -->
           <div v-if="this.user_id==post.user_id||this.currentUser.droits_id==2" class="update">
             <i @click="openUpdatePost" class="fas fa-ellipsis-h"></i>
             <div class="update__nav" v-if="isOpenPost">
@@ -25,9 +28,11 @@
             </div>
           </div>
         </div>
+        <!-- partie avec le text du post -->
         <div class="postText">
             <p class="postText__p">{{post.post_text}}</p>
         </div>  
+        <!-- partie avec la photo du post -->
         <div class="postPicture">
           <img class="postPicture__img" height="300" width="500" :src="post.post_picture">
         </div>
@@ -63,42 +68,6 @@
               :updateComment="updateComment"
 
                ></comment>
-              <!-- <comment></comment> -->
-              <!-- <div class="comment">
-                <div class="user user--center">
-                  <img  class="user__profilPicture " height="50" width="50" :src="this.currentUser.user_picture" alt="Image de profil ">
-                  <div  class="user__infos user__infos--comment">
-                        <p  class="user__fullName">Prénom Nom</p>
-                        <p  class="user__comment" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti unde ut nihil quas odio fuga iure amet. Consequuntur, quisquam quas.</p>
-                  </div>
-                  <div class="update">
-                      <i @click="openUpdateComment" class="fas fa-ellipsis-h"></i>
-                      <div class="update__nav" v-if="isOpenComment">
-                        <p class="update__update"><i class="far fa-edit"></i>Modifier</p>
-                        <p class="update__delete"><i class="far fa-trash-alt"></i>Supprimer</p>
-                      </div>
-                 </div>
-                </div>
-                <p class="user__date user__date--comment">le 14/01/2022</p>
-              </div> -->
-
-              <!-- <div class="comment">
-                <div class="user user--center">
-                  <img  class="user__profilPicture " height="50" width="50" :src="this.currentUser.user_picture" alt="Image de profil ">
-                  <div  class="user__infos user__infos--comment">
-                        <p  class="user__fullName">Prénom Nom</p>
-                        <p  class="user__comment" >Lorem ipsum </p>
-                  </div>
-                  <div class="update">
-                      <i @click="openUpdateComment" class="fas fa-ellipsis-h"></i>
-                      <div class="update__nav" v-if="isOpenComment">
-                        <p class="update__update"><i class="far fa-edit"></i>Modifier</p>
-                        <p class="update__delete"><i class="far fa-trash-alt"></i>Supprimer</p>
-                      </div>
-                 </div>
-                </div>
-                <p class="user__date user__date--comment">le 14/01/2022</p>
-              </div> -->
           </div>  
           <div class="separation"></div>
           <!-- ajouter un commentaire -->
@@ -128,13 +97,18 @@
 import { mapState } from 'vuex';
 import comment from '../components/comment.vue'
 import { mapGetters } from 'vuex';
+import updatePost from '../components/UpdatePost.vue'
+// import module Dayjs pour la gestion de l'affichage des dates 
+
 import dayjs from 'dayjs'
+// affichage en francais
 require("dayjs/locale/fr");
+// affichage de la durée depuis le dernier commentaire
 const  relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
 
-import updatePost from '../components/UpdatePost.vue'
+// import axios
 const axios = require('axios');
 // ajout d'une URL de base aux requetes
 const instance = axios.create({baseURL: 'http://localhost:3000/api'});
@@ -144,25 +118,34 @@ export default {
     components : {comment,updatePost} ,
     data: function(){
         return {
+          // photo qui vient d etre chargé
           previewPicture : null,
+          // ouverture du menu de Maj du post
           isOpenPost:false,
-          isOpenComment:false,
+          // ouverture du composant de update POST
           update:false,
+          // liste de like
           likeList:[],
+          // liste de commentaire
           commentList:[],
+          // texte du commentaire
           commentText:null,
+          // ouverture de la section commentaire
           openCommentSection:false
         }
     },
     props:['post','index'],
     
     computed:{
+      // nombre de like pour un post 
       likeNumber(){return this.likeList.length},
+      // nombre de commentaire pour un post
       CommentNumber(){return this.commentList.length},
+      // verification si l'utilisateur a liker la publication
       checklike() {
         return  this.likeList.findIndex(el => el.user_id == this.user_id);
-       
       },
+      // verification du texte saisi par l'utilisateur
       textValidation  () {
       const regexp = /[^a-zA-Z0-9.,!_@#\- ]/;
       if (regexp.test(this.commentText)) { return true}
@@ -171,29 +154,37 @@ export default {
       ...mapState(['currentUser','user_id']),...mapGetters(['fullName'])
     },
     methods:{
+      // ouverture de la section commentaire
       switchOpenCommentSection() {
         this.openCommentSection = !this.openCommentSection
       },
+      // ouverture du composant de MAJ du post
       updatePost (){this.update=!this.update},
+      // ouverture du menu de MAJ du post
       openUpdatePost : function(){this.isOpenPost = !this.isOpenPost},
-      openUpdateComment : function(){this.isOpenComment = !this.isOpenComment},
+      // mise en majuscule nom prenom
       capitalize(prenom,nom){
         return prenom[0].toUpperCase() + prenom.slice(1)+ " " + nom.toUpperCase()
       },
+      // formatage de la date de creation de la publication
       date(date){
         return dayjs(date).format('DD/MM/YYYY')
       },
+      // formatage de la date du commentaire
       fromNow(date){
         return dayjs(date).locale("fr").fromNow();
       },
+      // appel API pour supprimer un post 
       fetchDeletePost(){
         const post_id = this.post.post_id
+        // recuperation de l'index du post dans la liste de post du store
         const post_index =this.index;
         const self= this;
         instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
         instance.delete(`/post/${post_id}`)
            .then(function(res){
               console.log(res.data);
+              // MAJ du store 
               self.$store.commit("deletePost", post_index);                       
               })
             .catch(function(err){
@@ -201,36 +192,36 @@ export default {
               console.log(err.response.data)
               })                 
       },
+      // appel API pour recuperer la liste de like
       async getAllLike (){
         instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
         try{ 
-              // const post_id = this.post.post_id
               const post_id = this.post.post_id
-
-              console.log(post_id)
               // requete Get api pour recuperer la totalité des like pour le post
               const response = await instance.get(`/like/${post_id}`);
-              console.log("response.dataget all like////////");
-              console.log(response.data);
+              // Maj de la liste dans les datas du composant 
               this.likeList = response.data
 
         }
           catch(err) {
           console.log(err)
-          // commit("logout")
-        
+       
           
           }
      },
+    //  appel API pour enregistrer ou supprimer un like
       async toggleLike(){
        instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
        const post_id = this.post.post_id
+      //  verification si l utilisateur a liker le post
        const index = this.checklike
        const self =this;
+      //  si a deja liker le post
        if (index >=0) {
+        //  requete pour supprimer le like en BDD
          instance.delete(`/like/${post_id}`)
-           .then(function(res){
-              console.log(res.data);
+           .then(function(){
+              // MAJ de la liste dans les datas du composant
               self.likeList.splice(index,1);                    
               })
             .catch(function(err){
@@ -239,10 +230,12 @@ export default {
               })  
 
        }
+      //  si il n a pas encore liker
        else{
+        //  requete pour creer un like 
          instance.post(`/like/${post_id}`)
             .then(function(res){
-                console.log(res.data);
+                // MAJ de la liste dans les datas du composant
                 self.likeList.push(res.data.like)  
                         
             })
@@ -252,23 +245,22 @@ export default {
             })
        }
     },
+    // appel API pour recuperer la liste des commentaires
     async fetchListComment (){
       const post_id = this.post.post_id
       try{
         // requete Get api pour recuperer la totalité des commentaires relatifs au post
       instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
       const response = await instance.get(`/comment/${post_id}`);
-      console.log("response.data liste de commentaires////////");
-      console.log(response.data);
+      // MAJ de la liste dans les datas du composant
       this.commentList = response.data
       }
       catch(err) {
-        console.log(err)
-        // commit("logout")
-       
+        console.log(err)      
         
       }
     },
+  // appel API pour creer un nouveau commentaire
     async fetchNewComment(){
       if (this.textValidation||!this.commentText||this.commentText==" ") {return null}
       else {
@@ -276,11 +268,12 @@ export default {
           const text = this.commentText
           const self= this;
           const post_id = this.post.post_id
+          // requete  pour creer le post en BDD
           instance.post(`/comment/${post_id}`,{text})
                 .then(function(res){
-                    console.log(res.data);
+                    // creation objet commentaire avec le retour API
                     const newComment = {...res.data,user_picture:self.currentUser.user_picture,user_prenom:self.currentUser.user_prenom,user_nom:self.currentUser.user_nom };
-                    console.log(newComment)
+                    // MAJ de la liste dans les datas du composant
                     self.commentList.push(newComment)
                     self.commentText=null; 
                             
@@ -292,15 +285,16 @@ export default {
       }
       
     },
+    // suppression d'un commentaire dans les datas du composant
     deleteComment(index){
       this.commentList.splice(index,1);
     },
+    // MAJ d'un commentaire dans les datas du composant
     updateComment(index,data){
       this.commentList[index] = {...this.commentList[index], ...data}
     }
-    
-    // ...mapActions(['fetchCurrentUser'])
     },
+    // appel des fonctions de recuperation des likes et des commentaires dans le hook created
     created(){
      this.getAllLike();
      this.fetchListComment();
@@ -311,6 +305,8 @@ export default {
 
 <style lang="scss" scoped>
 
+
+// bloc post
 .post{
 
     display: flex;
@@ -323,7 +319,7 @@ export default {
       
     }
 }
-
+// partie avec les données du createur du post 
 .user{
   display: flex;
   position: relative;
@@ -337,13 +333,11 @@ export default {
   &__infos{
     margin-left: 1rem;
     display: flex;
-    
     flex-direction: column;
     justify-content: space-between;
     padding: 0.5rem;
     &--comment{
       width: 85%;
-      // height: ;
     }
 
   }
@@ -353,8 +347,6 @@ export default {
   }
   &__comment{
       width: 100%;
-      // margin-left: 1.5rem;
-      // border-radius: 2rem;
       background-color: $textarea  ;
       border: none;
       outline: none;
@@ -366,11 +358,6 @@ export default {
     font-size: 1.4rem;
     font-style: italic;
     color: $hint;
-    // &--comment{
-    //   font-size: 1.3rem;
-    //   font-style: italic;
-    //   margin-left: 7rem;
-    // }
   }
   &__newComment{
     position: absolute;
@@ -380,6 +367,7 @@ export default {
   
 }
 
+// partie pour initier la MAJ ou la suppression du post
 .update{
   display: flex;
   flex-direction: column;
@@ -389,7 +377,6 @@ export default {
     left: -22rem;
     top: 1rem;
     box-shadow: $box-shadow $border;
-    // background-color: $border;
     background-color: $primary;
     padding: 1rem;
     line-height: 3rem;
@@ -404,11 +391,13 @@ export default {
 
 }
 
+// partie avec le text du post
 .postText{
   margin: 1rem 0rem 0.5rem 0rem;
 
 }
 
+// partie avec la photo du post 
 .postPicture{
     margin: 1rem 0rem;
     display: flex;
@@ -416,6 +405,7 @@ export default {
     
 }
 
+// div de separation
 .separation{
   height: 1px;
   background-color: $border;
@@ -423,6 +413,8 @@ export default {
   
   
 }
+
+//  affichage du nb de like ainsi que du nombre de commentaires
 
 .numberContainer{
   display: flex;
@@ -453,7 +445,6 @@ export default {
   
 }
 .toggle{
-  // flex-grow: 1;
   flex : 1;
   text-align: center;
   padding: 1rem 0rem;
