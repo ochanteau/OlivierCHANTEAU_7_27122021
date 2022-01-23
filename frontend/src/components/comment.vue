f9688154ca0a36da152b0b35f48abc68dedbb24e
Olivier <chanteau.o@gmail.com>
2022-01-23 10:32:34 +0100

    clean component comment

--- frontend/src/components/comment.vue
+++ frontend/src/components/comment.vue
@@ -1,6 +1,9 @@
 <template>
+    
     <div class="comment">
+      <!-- container  -->
         <div class="user user--comment">
+            <!-- bloc avec les infos du commentaire -->
             <img  class="user__profilPicture " height="50" width="50" :src="this.comment.user_picture" alt="Image de profil ">
             <div  class="user__infos ">
                 <p  class="user__fullName">{{capitalize}}</p>
@@ -8,6 +11,7 @@
                 <textarea v-if="updateIsOpen"  v-model="newComment" aria-label="texte du commentaire" maxlength="100" class="user__comment" placeholder="Votre commentaire ?"  cols="30" rows="2"></textarea>
                 <i v-if="updateIsOpen" @click="fetchUpdateComment" class="fas fa-chevron-circle-right user__newComment"></i>
             </div>
+            <!-- bloc update du commentaire -->
             <div v-if="this.user_id==comment.user_id||this.currentUser.droits_id==2" class="update ">
                 <i @click="openUpdateComment" class="fas fa-ellipsis-h update__i"></i>
                 <div class="update__nav" v-if="openUpdate">
@@ -16,13 +20,17 @@
                 </div>
             </div>
         </div>
+        <!-- erreur -->
         <p class="error" v-if="this.textValidation">Le texte ne doit pas contenir de caracteres speciaux</p>
+        <!-- date du commentaire -->
         <p class="user__date ">{{fromNow}}</p>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+
+// import librairie axios
 const axios = require('axios');
 // ajout d'une URL de base aux requetes
 const instance = axios.create({baseURL: 'http://localhost:3000/api'});
@@ -31,41 +39,47 @@ const instance = axios.create({baseURL: 'http://localhost:3000/api'});
 
 export default {
    name:'comment',
-    // components : {Header} ,
     data: function(){
         return {
+          // texte du commenaire Maj
           newComment:this.comment.comment_text,
+          // variable d'ouverture de la modification du commentaire 
           updateIsOpen:false,
+          // variable d'ouverture du menu de modification du commentaire
           openUpdate:false,
          
         }
     },
     props:['comment','index','capitalize','fromNow','deleteComment','updateComment'],
-    created(){
-      console.log("created comment")
-    },
     computed:{
+      // validation du texte du commentaire
       textValidation  () {
       const regexp = /[^a-zA-Z0-9.,!_@#\- ]/;
-      if (regexp.test(this.newComment)) { return true}
-      else {return false} 
+        if (regexp.test(this.newComment)) { return true}
+        else {return false} 
     },
       ...mapState(['currentUser','user_id'])
     },
     methods:{
+      // passage au mode modification du commentaire
       switchUpdateIsOpen(){
         this.updateIsOpen=!this.updateIsOpen;
         this.openUpdate =false;
       },
+      // ouverture du menu de modification du commentaire
       openUpdateComment : function(){this.openUpdate = !this.openUpdate},
+      // suppression du post , maj serveur et component 
       fetchDeletePost(){
+        // recuperation de l id du commentaire 
         const comment_id = this.comment.comment_id
+        // recuperation de l index du commentaire dans la liste du component
         const comment_index =this.index;
         const self= this;
+        // requete de suppression au serveur 
         instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
         instance.delete(`/comment/${comment_id}`)
-           .then(function(res){
-              console.log(res.data);
+           .then(function(){
+              // MAJ des datas du component
               self.deleteComment(comment_index); 
               self.openUpdate= false;                    
               })
@@ -74,31 +88,31 @@ export default {
               console.log(err.response.data)
               })                 
       },
+      // requet de MAJ du commentaire en BDD et maj des datas du component
       async fetchUpdateComment(){
-      if (this.textValidation||!this.newComment||this.newComment==" ") {return null}
-      else {
-          instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
-          const text = this.newComment
-          const index =this.index
-          const self= this;
-          const comment_id = this.comment.comment_id
-          instance.put(`/comment/${comment_id}`,{text})
-                .then(function(res){
-                    console.log(res.data);
-                    self.updateComment(index,res.data);
-                    self.updateIsOpen=false;
-                    self.openUpdate=false;
-                            
-                })
-                .catch(function(err){
-                  console.log(err)
-                  console.log(err.response.data)
-                })
-      }
+        if (this.textValidation||!this.newComment||this.newComment==" ") {return null}
+        else {
+            instance.defaults.headers.common['Authorization'] =`Bearer ${localStorage.getItem('token')}`;
+            const text = this.newComment
+            const index =this.index
+            const self= this;
+            const comment_id = this.comment.comment_id
+            // Maj de la BDD
+            instance.put(`/comment/${comment_id}`,{text})
+                  .then(function(res){
+                      // maj du component 
+                      self.updateComment(index,res.data);
+                      self.updateIsOpen=false;
+                      self.openUpdate=false;
+                              
+                  })
+                  .catch(function(err){
+                    console.log(err)
+                    console.log(err.response.data)
+                  })
+        }
       },
-      // ...mapActions(['fetchCurrentUser'])
-    }
-    
+    }  
 }
 
 </script>
@@ -111,23 +125,17 @@ export default {
   display: flex;
   align-items: center;
   position: relative;
-    // &--comment{
-    //     align-items: flex-start;
-    // }
+  // bloc avec les infos du commentaire
   &__profilPicture{
     border-radius: 50%;
-    
   }
   &__infos{
     margin-left: 1rem;
     display: flex;
-    
     flex-direction: column;
     justify-content: space-between;
     padding: 0.5rem;
-   
-      width: 85%;
-
+    width: 85%;
   }
 
   &__fullName{
@@ -136,8 +144,6 @@ export default {
   }
   &__comment{
       width: 100%;
-      // margin-left: 1.5rem;
-      // border-radius: 2rem;
       background-color: $textarea  ;
       border: none;
       outline: none;
@@ -147,10 +153,9 @@ export default {
   }
   &__date{
     font-size: 1.4rem;
-    
-      font-size: 1.3rem;
-      font-style: italic;
-      margin-left: 7rem;
+    font-size: 1.3rem;
+    font-style: italic;
+     margin-left: 7rem;
    
   }
   &__newComment{
@@ -160,21 +165,17 @@ export default {
   }
 }
 
+//  bloc update du commentaire
 .update{
   display: flex;
   flex-direction: column;
   position: relative;
   padding-bottom: 3rem;
-//   &__i{
-//       vertical-align: top;
-//   }
   &__nav{
     position: absolute;
     left: -10rem;
-    // right: -10rem;
     top: 2rem;
     box-shadow: $box-shadow $border;
-    // background-color: $border;
     background-color: $primary;
     padding: 1rem;
     line-height: 3rem;
